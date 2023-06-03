546dc16d3b72da074a3fd284f783ef9c00af965b
Marcin Winiarski <winiardesign@gmail.com>
2023-06-03 18:52:46 +0200

    get all users from colletion

--- src/components/datatable/Datatable.jsx
+++ src/components/datatable/Datatable.jsx
@@ -2,10 +2,28 @@ import "./datatable.scss";
 import { DataGrid } from "@mui/x-data-grid";
 import { userColumns, userRows } from "../../datatablesource";
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import {db} from '../../firebase';
 
 const Datatable = () => {
-  const [data, setData] = useState(userRows);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    let list = [];
+    const fetchData = async () => {
+      const querySnapshot = await getDocs(collection(db, "users"));
+      try {
+       querySnapshot.forEach((doc) => {
+         list.push({id: doc.id, ...doc.data()})
+          });
+        setData(list);
+      } catch(err) {
+      console.log("something went wrong");
+      }
+    }
+    fetchData();
+  }, []);
 
   const handleDelete = (id) => {
     setData(data.filter((item) => item.id !== id));
